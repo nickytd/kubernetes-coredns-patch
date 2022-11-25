90b345187afc4fc28b452ae7009069444d5dd40d
Niki Dokovski <nickytd@gmail.com>
2022-11-25 14:27:41 +0100

    remove minikube

--- setup.sh
+++ setup.sh
@@ -6,13 +6,8 @@ dir=$(dirname $0)
 
 echo "apply coredns patch"
 
-IP=""
-if minikube status 2>&1 >/dev/null; then
-	IP=$(minikube ip)
-elif kind get clusters 2>&1 >/dev/null; then
-	IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
-fi
-
+kind get clusters 2>&1 >/dev/null;
+IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
 
 if [ -z $IP ]; then
 	echo "cannot get controle plane node ip"
@@ -22,11 +17,9 @@ fi
 echo "control-plane node ip: $IP"
 
 temp_dir=`mktemp -d`
-
 trap '{ rm -rf -- "$temp_dir"; }' EXIT
 sed "s/minikube_ip/$IP/g" $dir/hosts > $temp_dir/local.dev.hosts
 cp -r $dir/*.yaml $temp_dir/
-
 pushd $temp_dir
 
 while IFS= read -r line; do
